d3c6954f9b96eca55e19642288961f6cd95bb1ba
Emilia Garipova <emilia270627@gmail.com>
2020-05-05 23:41:14 +0300

    Added marker to the map

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import MapContainer from './MapContainer';
 import Markers from './Markers';
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className="content">
-        <MapContainer />
-        <Markers />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="content">
+                <MapContainer />
+                <Markers />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,33 +1,56 @@
 import React from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
 const mapStyles = {
-  width: '700px',
-  height: '700px'
+    width: '700px',
+    height: '700px'
 };
 
 const containerStyle = {
-  width: '700px',
-  height: '700px'
+    width: '700px',
+    height: '700px'
 }
 
 export class MapContainer extends React.Component {
-  render() {
-    return (
-      <Map
-        google={this.props.google}
-        zoom={11}
-        style={mapStyles}
-        containerStyle={containerStyle}
-        initialCenter={{
-          lat: 55.6132,
-          lng: 51.8470
-        }}
-      />
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            markers: [
+                { lat: 55.6132, lng: 51.8470 }
+            ],
+            center: {}
+        };
+    }
+
+    onCenterChanged = (mapProps, map) => {
+        debugger
+        let lat = map.center.lat();
+        let lng = map.center.lng();
+        this.setState({ center: { lat, lng } });
+    }
+
+    render() {
+        return (
+            <Map
+                google={this.props.google}
+                zoom={11}
+                style={mapStyles}
+                containerStyle={containerStyle}
+                initialCenter={{
+                    lat: 55.6132,
+                    lng: 51.8470
+                }}
+                onCenterChanged={this.onCenterChanged}
+            >
+                {this.state.markers.map(marker =>
+                    <Marker
+                        position={{ lat: marker.lat, lng: marker.lng }}
+                    />)}
+            </Map>
+        );
+    }
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyDrM-9kfb01hw-cavjmBiAzo4tDZPbUD6M'
+    apiKey: 'AIzaSyDrM-9kfb01hw-cavjmBiAzo4tDZPbUD6M'
 })(MapContainer);
